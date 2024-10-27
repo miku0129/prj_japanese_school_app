963e64b07ebfaeb741053751d3e0b2cddcd74444
miku0129 <svartkatt39@gmail.com>
2024-10-27 18:46:03 +0100

    refactor: update name of a route-folder

--- src/app/(materials)/particles/[category_id]/page.tsx
+++ src/app/(materials)/particles/[category_id]/page.tsx
@@ -1,8 +1,8 @@
 import QuestionBeginner from "../components/question-beginner/question-beginner.component";
 import { DATA } from "../data";
 
-export default function Page({ params }: { params: { id: string } }) {
-  const category_id = params.id;
+export default function Page({ params }: { params: { category_id: string } }) {
+  const category_id = params.category_id;
   const item = DATA.filter((item) => item.category === "particles").find(
     (item) => {
       return item.category_id === Number(category_id);
