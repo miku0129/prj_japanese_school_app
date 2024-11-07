f3b786d84e93722b95ce422fa8877a9d252e5ac5
miku0129 <svartkatt39@gmail.com>
2024-11-07 20:29:35 +0100

    feat: update functioning pass.component usting with data from db

--- src/app/(materials)/particles/[category_id]/result/page.tsx
+++ src/app/(materials)/particles/[category_id]/result/page.tsx
@@ -1,11 +1,34 @@
+import prisma from "@/lib/prisma";
 import Fail from "@/components/fail/fail.component";
 import Pass from "@/components/pass/pass.component";
 
-export default function Page({ searchParams }: { searchParams: SearchParams }) {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) {
+  const getQuestions = async () => {
+    const questions = await prisma.question.findMany({
+      include: {
+        additionalQuestion: true,
+        choices: {
+          include: {
+            choice: true,
+          },
+        },
+      },
+    });
+    return {
+      props: { questions },
+    };
+  };
+  const { props } = await getQuestions();
+  const questions = props.questions;
+
   const result = searchParams.state === "true" ? true : false;
   return (
     <div>
-      {result && <Pass />}
+      {result && <Pass params={questions} />}
       {!result && <Fail />}
     </div>
   );
