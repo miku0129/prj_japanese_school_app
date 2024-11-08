cc405a2b9d38d5de1cb1e25d9ce52526f7b7f67d
miku0129 <svartkatt39@gmail.com>
2024-11-08 18:00:40 +0100

    docs: update seed-data.ts

--- prisma/seed-data.ts
+++ prisma/seed-data.ts
@@ -63,28 +63,25 @@ export const SEEDS = [
       },
     ],
     additionalQuestion: {
-      phrase_front: "あめ",
-      phrase_back: "なめる",
+      phrase_front: "て",
+      phrase_back: "あらう",
     },
   },
   {
     id: 3,
-    character: "wo",
+    character: "ha",
     categoryId: 3,
+    level: "beginner",
+    isIndex: false,
     category: "particles",
-    level: "intermediate",
-    isIndex: true,
-    answer: "imouto wo yobu",
     choices: [
       {
         choice: {
           create: {
             id: 5,
-            character: "お",
-            en: "o",
-            image: "https://i.ibb.co/3TjcvyJ/img-o.png",
-            styleHeight: "50",
-            styleWidth: "50",
+            character: "わ",
+            en: "wa",
+            image: "https://i.ibb.co/Yj23XWD/img-wa.png",
           },
         },
       },
@@ -92,23 +89,33 @@ export const SEEDS = [
         choice: {
           create: {
             id: 6,
-            character: "を",
-            en: "wo",
-            image: "https://i.ibb.co/Gnyk250/img-wo.png",
-            styleHeight: "50",
-            styleWidth: "50",
+            character: "は",
+            en: "ha",
+            image: "https://i.ibb.co/zf7Qssr/img-ha.png",
           },
         },
       },
+    ],
+    additionalQuestion: {
+      phrase_front: "おばけ",
+      phrase_back: "こわい",
+    },
+  },
+  {
+    id: 4,
+    character: "ha",
+    categoryId: 4,
+    level: "beginner",
+    isIndex: false,
+    category: "particles",
+    choices: [
       {
         choice: {
           create: {
             id: 7,
-            character: "いもうと",
-            en: "imouto",
-            styleHeight: "200",
-            styleWidth: "50",
-            image: "https://i.ibb.co/kxKnG9P/img-imouto.jpg",
+            character: "は",
+            en: "ha",
+            image: "https://i.ibb.co/zf7Qssr/img-ha.png",
           },
         },
       },
@@ -116,23 +123,26 @@ export const SEEDS = [
         choice: {
           create: {
             id: 8,
-            character: "よぶ",
-            en: "yobu",
-            styleHeight: "100",
-            styleWidth: "50",
-            image: "https://i.ibb.co/SN8pcw8/img-yobu.jpg",
+            character: "わ",
+            en: "wa",
+            image: "https://i.ibb.co/Yj23XWD/img-wa.png",
           },
         },
       },
     ],
+    additionalQuestion: {
+      phrase_front: "ばった",
+      phrase_back: "むしです",
+    },
   },
+
   {
-    id: 4,
+    id: 5,
     character: "wo",
-    categoryId: 4,
+    categoryId: 5,
     category: "particles",
     level: "intermediate",
-    isIndex: false,
+    isIndex: true,
     answer: "imouto wo yobu",
     choices: [
       {
@@ -186,29 +196,233 @@ export const SEEDS = [
     ],
   },
   {
-    id: 5,
+    id: 6,
+    character: "wo",
+    categoryId: 6,
     category: "particles",
-    categoryId: 5,
+    level: "intermediate",
+    isIndex: false,
+    answer: "boushi wo kaburu",
+    choices: [
+      {
+        choice: {
+          create: {
+            id: 13,
+            character: "お",
+            en: "o",
+            image: "https://i.ibb.co/3TjcvyJ/img-o.png",
+            styleHeight: "50",
+            styleWidth: "50",
+          },
+        },
+      },
+      {
+        choice: {
+          create: {
+            id: 14,
+            character: "を",
+            en: "wo",
+            image: "https://i.ibb.co/Gnyk250/img-wo.png",
+            styleHeight: "50",
+            styleWidth: "50",
+          },
+        },
+      },
+      {
+        choice: {
+          create: {
+            id: 15,
+            character: "ぼうし",
+            en: "boushi",
+            styleHeight: "120",
+            styleWidth: "50",
+            image: "https://i.ibb.co/CVYRNfP/img-boushi.jpg",
+          },
+        },
+      },
+      {
+        choice: {
+          create: {
+            id: 16,
+            character: "かぶる",
+            en: "kaburu",
+            styleHeight: "120",
+            styleWidth: "50",
+            image: "https://i.ibb.co/9sZN0b1/img-kaburu.jpg",
+          },
+        },
+      },
+    ],
+  },
+  {
+    id: 7,
+    character: "ha",
+    categoryId: 7,
+    category: "particles",
+    level: "intermediate",
+    isIndex: false,
+    answer: "natu ha atui",
+    choices: [
+      {
+        choice: {
+          create: {
+            id: 17,
+            character: "なつ",
+            en: "natu",
+            image: "https://i.ibb.co/ts8myMQ/img-natu.jpg",
+            styleHeight: "100",
+            styleWidth: "50",
+          },
+        },
+      },
+      {
+        choice: {
+          create: {
+            id: 18,
+            character: "わ",
+            en: "wa",
+            image: "https://i.ibb.co/Yj23XWD/img-wa.png",
+            styleHeight: "50",
+            styleWidth: "50",
+          },
+        },
+      },
+      {
+        choice: {
+          create: {
+            id: 19,
+            character: "は",
+            en: "ha",
+            styleHeight: "50",
+            styleWidth: "50",
+            image: "https://i.ibb.co/zf7Qssr/img-ha.png",
+          },
+        },
+      },
+      {
+        choice: {
+          create: {
+            id: 20,
+            character: "あつい",
+            en: "atui",
+            styleHeight: "120",
+            styleWidth: "50",
+            image: "https://i.ibb.co/bPqRzXW/img-atui.jpg",
+          },
+        },
+      },
+    ],
+  },
+  {
+    id: 8,
+    character: "ha",
+    categoryId: 8,
+    category: "particles",
+    level: "intermediate",
+    isIndex: false,
+    answer: "kuruma ha hashiru",
+    choices: [
+      {
+        choice: {
+          create: {
+            id: 21,
+            character: "はしる",
+            en: "hashiru",
+            image: "https://i.ibb.co/fr7gNYS/img-hashiru.jpg",
+            styleHeight: "120",
+            styleWidth: "50",
+          },
+        },
+      },
+      {
+        choice: {
+          create: {
+            id: 22,
+            character: "わ",
+            en: "wa",
+            image: "https://i.ibb.co/Yj23XWD/img-wa.png",
+            styleHeight: "50",
+            styleWidth: "50",
+          },
+        },
+      },
+      {
+        choice: {
+          create: {
+            id: 23,
+            character: "は",
+            en: "ha",
+            styleHeight: "50",
+            styleWidth: "50",
+            image: "https://i.ibb.co/zf7Qssr/img-ha.png",
+          },
+        },
+      },
+      {
+        choice: {
+          create: {
+            id: 24,
+            character: "くるま",
+            en: "kuruma",
+            styleHeight: "120",
+            styleWidth: "50",
+            image: "https://i.ibb.co/2M6DcKc/img-kuruma.jpg",
+          },
+        },
+      },
+    ],
+  },
+
+  {
+    id: 9,
+    category: "particles",
+    categoryId: 9,
     level: "advanced",
     isIndex: true,
     answer: "おにはこわい",
     choices: [],
     additionalQuestion: {
       sound_resource:
-        "https://dl.dropboxusercontent.com/scl/fi/ybe00sx6hc87nhycica3n/onihakowai.m4a?rlkey=hcdddo8i2mjn7cl45ptkatwgs&st=gyzsimc9&dl=0",
+        "https://dl.dropboxusercontent.com/scl/fi/ybe00sx6hc87nhycica3n/onihakowai.m4a?rlkey=hcdddo8i2mjn7cl45ptkatwgs&st=8qnwuots&dl=0",
     },
   },
   {
-    id: 6,
+    id: 10,
     category: "particles",
-    categoryId: 6,
+    categoryId: 10,
     level: "advanced",
     isIndex: false,
-    answer: "おにはこわい",
+    answer: "おかしをたべる",
     choices: [],
     additionalQuestion: {
       sound_resource:
-        "https://dl.dropboxusercontent.com/scl/fi/ybe00sx6hc87nhycica3n/onihakowai.m4a?rlkey=hcdddo8i2mjn7cl45ptkatwgs&st=gyzsimc9&dl=0",
+        "https://dl.dropboxusercontent.com/scl/fi/058e25tmspifom94vep3h/okashiwotaberu.m4a?rlkey=yynjlfxfoj69eekq7cgd7njza&st=edqqtisn&dl=0",
+    },
+  },
+  {
+    id: 11,
+    category: "particles",
+    categoryId: 11,
+    level: "advanced",
+    isIndex: false,
+    answer: "バナナはおいしい",
+    choices: [],
+    additionalQuestion: {
+      sound_resource:
+        "https://dl.dropboxusercontent.com/scl/fi/1f77r5cj6g4bethfmydwx/bananahaoishii.m4a?rlkey=3g5bdu36lxdc92l5tbjov8arr&st=xp6xhwev&dl=0",
+    },
+  },
+  {
+    id: 12,
+    category: "particles",
+    categoryId: 12,
+    level: "advanced",
+    isIndex: false,
+    answer: "ろうそくをけす",
+    choices: [],
+    additionalQuestion: {
+      sound_resource:
+        "https://dl.dropboxusercontent.com/scl/fi/8u0gnh3il6bc5nhufn6z0/rousokuwokesu.m4a?rlkey=xegb85a6zc6zkcagu4f3kqh4r&st=3x0c6t6t&dl=0",
     },
   },
 ];
