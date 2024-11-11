9c0e034b82169b8d5b2b69bbe2c8b2f6f83f06aa
miku0129 <svartkatt39@gmail.com>
2024-11-11 23:03:26 +0100

    feat: update questno-advanced.component.tsx

--- src/app/(materials)/particles/components/question-advanced/question-advanced.component.tsx
+++ src/app/(materials)/particles/components/question-advanced/question-advanced.component.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import Audio from "@/components/audio/audio.component";
 import {
   CustomPhraseStyle,
-  CustomIconBtnStyle,
-  CustomBtnContainerStyle,
+  // CustomIconBtnStyle,
+  // CustomBtnContainerStyle,
 } from "@/styles/styled-components/page";
 import styles from "./question-advanced.style.module.scss";
 
@@ -15,112 +15,89 @@ export default function QuestionAdvanced({
 }: {
   params: Question;
 }) {
-  const [questions, setQuestions] = useState<Question[]>([]);
-  const [showAnswer, setShowAnswer] = useState(false);
-  const router = useRouter();
+  // const [questions, setQuestions] = useState<Question[]>([]);
+  // const [showAnswer, setShowAnswer] = useState(false);
+
+  const [content, setContent] = useState("");
+
+  // const router = useRouter();
 
   useEffect(() => {
     async function getQuestions() {
-      const res = await fetch(`/api/particles/`, {
-        method: "GET",
-      });
-      const data = await res.json();
-      setQuestions(data)
+      // const res = await fetch(`/api/particles/`, {
+      //   method: "GET",
+      // });
+      // const data = await res.json();
+      // setQuestions(data)
     }
     getQuestions();
   }, []);
 
-  useEffect(() => {
-    if (question.isIndex) {
-      window.alert(
-        "きこえた文をかいてみよう。かけたら👁を押してこたえをかくにんしよう。"
-      );
-    }
-  }, [question.isIndex]);
+  // useEffect(() => {
+  //   if (question.isIndex) {
+  //     window.alert(
+  //       "きこえた文をかいてみよう。かけたら👁を押してこたえをかくにんしよう。"
+  //     );
+  //   }
+  // }, [question.isIndex]);
 
-  function answerHandler() {
-    setShowAnswer(!showAnswer);
-  }
+  // function answerHandler() {
+  //   setShowAnswer(!showAnswer);
+  // }
 
-  function pageHandler() {
-    const nextCategoryId = question.categoryId + 1;
-    router.push(`/particles/${nextCategoryId}/?level=${question.level}`);
-  }
+  // function pageHandler() {
+  //   const nextCategoryId = question.categoryId + 1;
+  //   router.push(`/particles/${nextCategoryId}/?level=${question.level}`);
+  // }
 
-  const getMessage = () => {
-    window.alert("💯クリアおめでとう！");
-    router.push("/");
-  };
+  // const getMessage = () => {
+  //   window.alert("💯クリアおめでとう！");
+  //   router.push("/");
+  // };
 
   return (
-    <div className={styles.sample}>
-      <div className={styles.phraseContainer}>
-        <CustomPhraseStyle>
-          <p>👂</p>
-          <p>音</p>
-          <p>を</p>
-          <p>き</p>
-          <p>き</p>
-          <p>と</p>
-          <p>ろ</p>
-          <p>う</p>
-        </CustomPhraseStyle>
+    // <div className={styles.sample}>
+    <div className={styles.phraseContainer}>
+      <CustomPhraseStyle>
+        <p>👂</p>
+        <p>音</p>
+        <p>を</p>
+        <p>き</p>
+        <p>き</p>
+        <p>と</p>
+        <p>ろ</p>
+        <p>う</p>
+      </CustomPhraseStyle>
 
-        <div className={styles.answerOuterContainer}>
-          {showAnswer && (
-            <div className={styles.answerBoard}>
-              <div className={styles.explanation}>
-                <p>こ</p>
-                <p>た</p>
-                <p>え</p>
-              </div>
-              <div className={styles.answerPhraseContainer}>
-                {question.answer!.split("").map((char: string, idx: number) => {
-                  return <p key={idx}>{char}</p>;
-                })}
-              </div>
-            </div>
-          )}
-          {!showAnswer && (
-            <CustomBtnContainerStyle>
-              <CustomIconBtnStyle onClick={() => answerHandler()}>
-                <i className="fa-regular fa-eye"></i>
-              </CustomIconBtnStyle>
-            </CustomBtnContainerStyle>
-          )}
-          {showAnswer && questions.length > question.categoryId && (
-            <div className={styles.buttonContainer}>
-              <CustomBtnContainerStyle>
-                <CustomIconBtnStyle onClick={() => answerHandler()}>
-                  <i className="fa-regular fa-eye-slash"></i>
-                </CustomIconBtnStyle>
-              </CustomBtnContainerStyle>
-              <CustomBtnContainerStyle>
-                <CustomIconBtnStyle onClick={() => pageHandler()}>
+      <div className={styles.formContainer}>
+        <div className={styles.formOuter}>
+          <form onSubmit={() => {}}>
+            <div className={styles.formIner}>
+              <textarea
+                cols={30}
+                onChange={(e) => setContent(e.target.value)}
+                placeholder="こたえをかいてね"
+                rows={2}
+                value={content}
+                className={styles.inputField}
+              />
+              <div className={styles.btnContainer}>
+                <button
+                  disabled={!content}
+                  type="submit"
+                  className={styles.inputBtn}
+                >
                   <i className="fa-solid fa-arrow-left"></i>
-                </CustomIconBtnStyle>
-              </CustomBtnContainerStyle>
+                </button>
+              </div>
             </div>
-          )}
-          {showAnswer && questions.length === question.categoryId && (
-            <div className={styles.buttonContainer}>
-              <CustomBtnContainerStyle>
-                <CustomIconBtnStyle onClick={() => answerHandler()}>
-                  <i className="fa-regular fa-eye-slash"></i>
-                </CustomIconBtnStyle>
-              </CustomBtnContainerStyle>
-              <CustomBtnContainerStyle>
-                <CustomIconBtnStyle onClick={() => getMessage()}>
-                  <i className="fa-regular fa-face-smile-wink"></i>
-                </CustomIconBtnStyle>
-              </CustomBtnContainerStyle>
-            </div>
-          )}
+          </form>
         </div>
+        {question.additionalQuestion?.sound_resource && (
+          <Audio audioLink={question.additionalQuestion!.sound_resource} />
+        )}
       </div>
-      {question.additionalQuestion?.sound_resource && (
-        <Audio audioLink={question.additionalQuestion!.sound_resource} />
-      )}
     </div>
+    // </div>
   );
 }
