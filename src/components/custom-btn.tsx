f1bfcc58d793d2b73003ee501618e2ce79265b3e
miku0129 <svartkatt39@gmail.com>
2025-04-01 11:23:48 +0200

    refactor: updating styles for ParticlesMenuBoard

--- src/components/custom-btn.tsx
+++ src/components/custom-btn.tsx
@@ -6,7 +6,7 @@ export default function CustomBtn(props: Props) {
   const { children, className, ...rest } = props;
   return (
     <button
-      className={`h-28 text-3xl bg-gray-300 border-0 rounded-lg py-4 px-8 cursor-pointer ${
+      className={`h-28 text-2xl md:text-3xl bg-gray-300 border-0 rounded-lg py-4 px-5 sm:px-8 cursor-pointer ${
         className ? className : ""
       }`}
       {...rest}
