3b1862f9bf981ceae222b797163ab8d891364a75
miku0129 <svartkatt39@gmail.com>
2025-04-13 23:17:29 +0200

    refactor: updating seeding to Particles

---
+++ prisma/particles-seed-data.ts
@@ -0,0 +1,422 @@
+export const PARTICLES_SEED_DATA = [
+  {
+    character: "wo",
+    group: "beginner",
+    isIndex: true,
+    category: "particles",
+    options: [
+      {
+        character: "お",
+        en: "o",
+        image: "https://i.ibb.co/HxwYW1p/img-o.png",
+      },
+      {
+        character: "を",
+        en: "wo",
+        image: "https://i.ibb.co/Zcwy4qv/img-wo.png",
+      },
+    ],
+    additionalQuestion: {
+      phrase_front: "あめ",
+      phrase_back: "なめる",
+    },
+  },
+  {
+    character: "wo",
+    group: "beginner",
+    isIndex: false,
+    category: "particles",
+    options: [
+      {
+        character: "お",
+        en: "o",
+        image: "https://i.ibb.co/HxwYW1p/img-o.png",
+      },
+      {
+        character: "を",
+        en: "wo",
+        image: "https://i.ibb.co/Zcwy4qv/img-wo.png",
+      },
+    ],
+    additionalQuestion: {
+      phrase_front: "て",
+      phrase_back: "あらう",
+    },
+  },
+  {
+    character: "ha",
+    group: "beginner",
+    isIndex: false,
+    category: "particles",
+    options: [
+      {
+        character: "わ",
+        en: "wa",
+        image: "https://i.ibb.co/ft3G8n3/img-wa.png",
+      },
+      {
+        character: "は",
+        en: "ha",
+        image: "https://i.ibb.co/12zdtgf/img-ha.png",
+      },
+    ],
+    additionalQuestion: {
+      phrase_front: "おばけ",
+      phrase_back: "こわい",
+    },
+  },
+  {
+    character: "he",
+    group: "beginner",
+    isIndex: false,
+    category: "particles",
+    options: [
+      {
+        character: "へ",
+        en: "he",
+        image: "https://i.ibb.co/09vRrS6/img-he.png",
+      },
+      {
+        character: "え",
+        en: "e",
+        image: "https://i.ibb.co/myQxNYc/img-e.png",
+      },
+    ],
+    additionalQuestion: {
+      phrase_front: "がっこう",
+      phrase_back: "いく",
+    },
+  },
+  {
+    character: "he",
+    group: "beginner",
+    isIndex: false,
+    category: "particles",
+    options: [
+      {
+        character: "へ",
+        en: "he",
+        image: "https://i.ibb.co/09vRrS6/img-he.png",
+      },
+      {
+        character: "え",
+        en: "e",
+        image: "https://i.ibb.co/myQxNYc/img-e.png",
+      },
+    ],
+    additionalQuestion: {
+      phrase_front: "まえ",
+      phrase_back: "すすむ",
+    },
+  },
+  {
+    character: "ha",
+    group: "beginner",
+    isIndex: false,
+    category: "particles",
+    options: [
+      {
+        character: "は",
+        en: "ha",
+        image: "https://i.ibb.co/12zdtgf/img-ha.png",
+      },
+      {
+        character: "わ",
+        en: "wa",
+        image: "https://i.ibb.co/ft3G8n3/img-wa.png",
+      },
+    ],
+    additionalQuestion: {
+      phrase_front: "ばった",
+      phrase_back: "むしです",
+    },
+  },
+  {
+    character: "wo",
+    category: "particles",
+    group: "intermediate",
+    isIndex: true,
+    answer: "imouto wo yobu",
+    options: [
+      {
+        character: "お",
+        en: "o",
+        image: "https://i.ibb.co/HxwYW1p/img-o.png",
+        styleHeight: "50",
+        styleWidth: "50",
+      },
+      {
+        character: "を",
+        en: "wo",
+        image: "https://i.ibb.co/Zcwy4qv/img-wo.png",
+        styleHeight: "50",
+        styleWidth: "50",
+      },
+      {
+        character: "いもうと",
+        en: "imouto",
+        styleHeight: "200",
+        styleWidth: "50",
+        image: "https://i.ibb.co/31GDYmS/img-imouto.jpg",
+      },
+      {
+        character: "よぶ",
+        en: "yobu",
+        styleHeight: "100",
+        styleWidth: "50",
+        image: "https://i.ibb.co/K6Z6kZj/img-yobu.jpg",
+      },
+    ],
+  },
+  {
+    character: "he",
+    category: "particles",
+    group: "intermediate",
+    isIndex: false,
+    answer: "migi he ugoku",
+    options: [
+      {
+        character: "うごく",
+        en: "ugoku",
+        image: "https://i.ibb.co/m9Z9Fnp/img-ugoku.jpg",
+        styleHeight: "120",
+        styleWidth: "50",
+      },
+      {
+        character: "へ",
+        en: "he",
+        image: "https://i.ibb.co/09vRrS6/img-he.png",
+        styleHeight: "50",
+        styleWidth: "50",
+      },
+      {
+        character: "え",
+        en: "e",
+        styleHeight: "50",
+        styleWidth: "50",
+        image: "https://i.ibb.co/myQxNYc/img-e.png",
+      },
+      {
+        character: "みぎ",
+        en: "migi",
+        styleHeight: "100",
+        styleWidth: "50",
+        image: "https://i.ibb.co/Ldjwnrc/img-migi.jpg",
+      },
+    ],
+  },
+  {
+    character: "wo",
+    category: "particles",
+    group: "intermediate",
+    isIndex: false,
+    answer: "boushi wo kaburu",
+    options: [
+      {
+        character: "お",
+        en: "o",
+        image: "https://i.ibb.co/HxwYW1p/img-o.png",
+        styleHeight: "50",
+        styleWidth: "50",
+      },
+      {
+        character: "を",
+        en: "wo",
+        image: "https://i.ibb.co/Zcwy4qv/img-wo.png",
+        styleHeight: "50",
+        styleWidth: "50",
+      },
+      {
+        character: "ぼうし",
+        en: "boushi",
+        styleHeight: "120",
+        styleWidth: "50",
+        image: "https://i.ibb.co/D4JHTLM/img-boushi.jpg",
+      },
+      {
+        character: "かぶる",
+        en: "kaburu",
+        styleHeight: "120",
+        styleWidth: "50",
+        image: "https://i.ibb.co/qg1zCQp/img-kaburu.jpg",
+      },
+    ],
+  },
+  {
+    character: "ha",
+    category: "particles",
+    group: "intermediate",
+    isIndex: false,
+    answer: "natu ha atui",
+    options: [
+      {
+        character: "なつ",
+        en: "natu",
+        image: "https://i.ibb.co/VVnCcg8/img-natu.jpg",
+        styleHeight: "100",
+        styleWidth: "50",
+      },
+      {
+        character: "わ",
+        en: "wa",
+        image: "https://i.ibb.co/ft3G8n3/img-wa.png",
+        styleHeight: "50",
+        styleWidth: "50",
+      },
+      {
+        character: "は",
+        en: "ha",
+        styleHeight: "50",
+        styleWidth: "50",
+        image: "https://i.ibb.co/12zdtgf/img-ha.png",
+      },
+      {
+        character: "あつい",
+        en: "atui",
+        styleHeight: "120",
+        styleWidth: "50",
+        image: "https://i.ibb.co/0K6tv70/img-atui.jpg",
+      },
+    ],
+  },
+  {
+    character: "ha",
+    category: "particles",
+    group: "intermediate",
+    isIndex: false,
+    answer: "kuruma ha hashiru",
+    options: [
+      {
+        character: "はしる",
+        en: "hashiru",
+        image: "https://i.ibb.co/bXY2wGB/img-hashiru.jpg",
+        styleHeight: "120",
+        styleWidth: "50",
+      },
+      {
+        character: "わ",
+        en: "wa",
+        image: "https://i.ibb.co/ft3G8n3/img-wa.png",
+        styleHeight: "50",
+        styleWidth: "50",
+      },
+      {
+        character: "は",
+        en: "ha",
+        styleHeight: "50",
+        styleWidth: "50",
+        image: "https://i.ibb.co/12zdtgf/img-ha.png",
+      },
+      {
+        character: "くるま",
+        en: "kuruma",
+        styleHeight: "120",
+        styleWidth: "50",
+        image: "https://i.ibb.co/wM0Bvxr/img-kuruma.jpg",
+      },
+    ],
+  },
+  {
+    character: "he",
+    category: "particles",
+    group: "intermediate",
+    isIndex: false,
+    answer: "ushiro he sagaru",
+    options: [
+      {
+        character: "へ",
+        en: "he",
+        image: "https://i.ibb.co/09vRrS6/img-he.png",
+        styleHeight: "50",
+        styleWidth: "50",
+      },
+      {
+        character: "さがる",
+        en: "sagaru",
+        image: "https://i.ibb.co/m9Z9Fnp/img-ugoku.jpg",
+        styleHeight: "120",
+        styleWidth: "50",
+      },
+      {
+        character: "え",
+        en: "e",
+        styleHeight: "50",
+        styleWidth: "50",
+        image: "https://i.ibb.co/myQxNYc/img-e.png",
+      },
+      {
+        character: "うしろ",
+        en: "ushiro",
+        styleHeight: "120",
+        styleWidth: "50",
+        image: "https://i.ibb.co/WHsYyCb/img-ushiro.jpg",
+      },
+    ],
+  },
+  {
+    category: "particles",
+    group: "advanced",
+    isIndex: true,
+    answer: "うちゅうへいく",
+    options: [],
+    additionalQuestion: {
+      sound_resource:
+        "https://dl.dropboxusercontent.com/scl/fi/2eqfql8sqx7i2tfmybhtw/uchyuheiku.m4a?rlkey=36idrjqz2qkjoja2zqh07esnl&st=x8a82nvr&dl=0",
+    },
+  },
+  {
+    category: "particles",
+    group: "advanced",
+    isIndex: false,
+    answer: "おにはこわい",
+    options: [],
+    additionalQuestion: {
+      sound_resource:
+        "https://dl.dropboxusercontent.com/scl/fi/ybe00sx6hc87nhycica3n/onihakowai.m4a?rlkey=hcdddo8i2mjn7cl45ptkatwgs&st=8qnwuots&dl=0",
+    },
+  },
+  {
+    category: "particles",
+    group: "advanced",
+    isIndex: false,
+    answer: "ひだりへとぶ",
+    options: [],
+    additionalQuestion: {
+      sound_resource:
+        "https://dl.dropboxusercontent.com/scl/fi/z3d7njvi3a23zkaquxplt/hidarihetobu.m4a?rlkey=qvwareez8az4j9aalc0sicz4l&st=o896ygmb&dl=0",
+    },
+  },
+  {
+    category: "particles",
+    group: "advanced",
+    isIndex: false,
+    answer: "おかしをたべる",
+    options: [],
+    additionalQuestion: {
+      sound_resource:
+        "https://dl.dropboxusercontent.com/scl/fi/058e25tmspifom94vep3h/okashiwotaberu.m4a?rlkey=yynjlfxfoj69eekq7cgd7njza&st=edqqtisn&dl=0",
+    },
+  },
+  {
+    category: "particles",
+    group: "advanced",
+    isIndex: false,
+    answer: "ばななはおいしい",
+    options: [],
+    additionalQuestion: {
+      sound_resource:
+        "https://dl.dropboxusercontent.com/scl/fi/1f77r5cj6g4bethfmydwx/bananahaoishii.m4a?rlkey=3g5bdu36lxdc92l5tbjov8arr&st=xp6xhwev&dl=0",
+    },
+  },
+  {
+    category: "particles",
+    group: "advanced",
+    isIndex: false,
+    answer: "ろうそくをけす",
+    options: [],
+    additionalQuestion: {
+      sound_resource:
+        "https://dl.dropboxusercontent.com/scl/fi/8u0gnh3il6bc5nhufn6z0/rousokuwokesu.m4a?rlkey=xegb85a6zc6zkcagu4f3kqh4r&st=3x0c6t6t&dl=0",
+    },
+  },
+];
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,71 +1,74 @@
 import { PrismaClient } from "@prisma/client";
-import { SEEDS } from "./seed-data";
+import { PARTICLES_SEED_DATA } from "./particles-seed-data";
 
 const prisma = new PrismaClient();
-const seedData = SEEDS;
 
 async function main() {
-  console.log("Seeding data...");
-  let id = 0;
-  let category_id = 0;
-  let option_id = 0;
-  let options = [];
+  (async function () {
+    console.log("Seeding PARTICLES_SEED_DATA...");
+    
+    const seedData = PARTICLES_SEED_DATA;
+    let id = 0;
+    let category_id = 0;
+    let option_id = 0;
+    let options = [];
 
-  for (let i = 0; i < seedData.length; i++) {
-    if (seedData[i].options[0]) {
-      for (let k = 0; k < seedData[i].options.length; k++) {
-        options[k] = {
-          option: {
-            create: {
-              id: option_id,
-              ...seedData[i].options[k],
+    for (let i = 0; i < seedData.length; i++) {
+      if (seedData[i].options[0]) {
+        for (let k = 0; k < seedData[i].options.length; k++) {
+          options[k] = {
+            option: {
+              create: {
+                id: option_id,
+                ...seedData[i].options[k],
+              },
             },
-          },
-        };
-        option_id++;
+          };
+          option_id++;
+        }
+      } else {
+        options = [];
       }
-    } else {
-      options = [];
-    }
 
-    if (seedData[i].additionalQuestion) {
-      await prisma.particlesQuestion.create({
-        data: {
-          id: id,
-          category: seedData[i].category,
-          categoryId: category_id,
-          group: seedData[i].group,
-          isIndex: seedData[i].isIndex,
-          options: {
-            create: options,
+      if (seedData[i].additionalQuestion) {
+        await prisma.particlesQuestion.create({
+          data: {
+            id: id,
+            category: seedData[i].category,
+            categoryId: category_id,
+            group: seedData[i].group,
+            isIndex: seedData[i].isIndex,
+            options: {
+              create: options,
+            },
+            additionalQuestion: {
+              create: seedData[i].additionalQuestion,
+            },
+            character: seedData[i].character,
+            answer: seedData[i].answer,
           },
-          additionalQuestion: {
-            create: seedData[i].additionalQuestion,
+        });
+      } else if (!seedData[i].additionalQuestion) {
+        await prisma.particlesQuestion.create({
+          data: {
+            id: id,
+            category: seedData[i].category,
+            categoryId: category_id,
+            group: seedData[i].group,
+            isIndex: seedData[i].isIndex,
+            options: {
+              create: options,
+            },
+            character: seedData[i].character,
+            answer: seedData[i].answer,
           },
-          character: seedData[i].character,
-          answer: seedData[i].answer,
-        },
-      });
-    } else if (!seedData[i].additionalQuestion) {
-      await prisma.particlesQuestion.create({
-        data: {
-          id: id,
-          category: seedData[i].category,
-          categoryId: category_id,
-          group: seedData[i].group,
-          isIndex: seedData[i].isIndex,
-          options: {
-            create: options,
-          },
-          character: seedData[i].character,
-          answer: seedData[i].answer,
-        },
-      });
+        });
+      }
+      id++;
+      category_id++;
     }
-    id++;
-    category_id++;
-  }
-  console.log("Data seeding complete.");
+    console.log("PARTICLES_SEED_DATA seeding complete.");
+  })();
 }
 
 main()
