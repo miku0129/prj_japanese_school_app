ca9a3dc3fb18d3b1e53a74b6101e236d9e916b89
miku0129 <svartkatt39@gmail.com>
2025-04-14 17:19:04 +0200

    refactor: updating seeding for Particles

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,25 +7,21 @@ const prisma = new PrismaClient();
 async function main() {
   (async function () {
     console.log("Seeding PARTICLES_SEED_DATA...");
-    
+
     const seedData = PARTICLES_SEED_DATA;
-    let id = 0;
     let category_id = 0;
-    let option_id = 0;
-    let options = [];
 
     for (let i = 0; i < seedData.length; i++) {
+      let options = [];
       if (seedData[i].options[0]) {
         for (let k = 0; k < seedData[i].options.length; k++) {
           options[k] = {
             option: {
               create: {
-                id: option_id,
                 ...seedData[i].options[k],
               },
             },
           };
-          option_id++;
         }
       } else {
         options = [];
@@ -34,7 +30,6 @@ async function main() {
       if (seedData[i].additionalQuestion) {
         await prisma.particlesQuestion.create({
           data: {
-            id: id,
             category: seedData[i].category,
             categoryId: category_id,
             group: seedData[i].group,
@@ -52,7 +47,6 @@ async function main() {
       } else if (!seedData[i].additionalQuestion) {
         await prisma.particlesQuestion.create({
           data: {
-            id: id,
             category: seedData[i].category,
             categoryId: category_id,
             group: seedData[i].group,
@@ -65,7 +59,6 @@ async function main() {
           },
         });
       }
-      id++;
       category_id++;
     }
     console.log("PARTICLES_SEED_DATA seeding complete.");
