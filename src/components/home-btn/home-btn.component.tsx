746fa6264a9e6ac5d3236e91942bfdd5055e14bc
miku0129 <svartkatt39@gmail.com>
2025-06-03 16:29:11 +0200

    refactor: updating particles-menu-btn and home-btn

--- src/components/home-btn/home-btn.component.tsx
+++ src/components/home-btn/home-btn.component.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import CustomBtn from "../custom-btn";
+import { Button } from "@msano/prj_msano_lib";
 
 export default function HomeBtn({ params: index }: { params: HomeIndex }) {
   const router = useRouter();
   return (
-    <CustomBtn onClick={() => router.push(`/${index.en}`)} className="text-white">
+    <Button size="lg" onClick={() => router.push(`/${index.en}`)}>
       {index.sample}をまなぶ
-    </CustomBtn>
+    </Button>
   );
 }
